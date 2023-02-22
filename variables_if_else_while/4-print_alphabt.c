d32eccaedd59fd344d5be96ae77e496eb45fade1
mau4512 <mauricio.25.17@gmail.com>
2023-02-22 12:51:25 -0800

    ejercicio 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'e' && i != 'q')
+			putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
