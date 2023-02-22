c6b654daabaf12275fafc7e04337d989d2011ad3
mau4512 <mauricio.25.17@gmail.com>
2023-02-22 13:13:18 -0800

    ejercicio 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+	
+	for (i = 'z'; i >= 'a'; i--)
+	       putchar(i);
+	putchar('\n');
+	return (0);	
+}
