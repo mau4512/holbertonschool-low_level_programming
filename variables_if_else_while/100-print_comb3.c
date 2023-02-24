fe0b3c28c47c17033e02394d3cd38c95a94780bb
mau4512 <mauricio.25.17@gmail.com>
2023-02-24 10:39:16 -0800

    ejercicio 10

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -14,15 +14,15 @@ int main(void)
 
 	int j;
 
-	for (i = 0; i<10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j=1; j<10; j++)
+		for (j = 1; j < 10; j++)
 		{
-			if((i!=j)&&(j>i))
+			if ((i != j) && (j > i))
 			{
-				putchar(i + '0');
-				putchar(j + '0');
-				if(i<9 || j<8)
+				putchar (i + '0');
+				putchar (j + '0');
+				if (i != 8 || j != 9)
 				{
 					putchar(',');
 					putchar(' ');
@@ -31,7 +31,7 @@ int main(void)
 		}
 
 	}
-	putchar('\n');
+	putchar ('\n');
 
-	return(0);
+	return (0);
 }
