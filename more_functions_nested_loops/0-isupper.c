ad8c95c076f065b17d002191bdb8d78276af14e2
mau4512 <mauricio.25.17@gmail.com>
2023-02-28 07:11:48 -0800

    ejercicio 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return 1;
+	return 0;
+}
