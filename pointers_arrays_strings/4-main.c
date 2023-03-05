d8997064dc32cd847285be9d60159ffece581209
mau4512 <mauricio.25.17@gmail.com>
2023-03-04 17:01:10 -0800

    ejercicio 4

--- pointers_arrays_strings/4-main.c
+++ pointers_arrays_strings/4-main.c
@@ -1,15 +1,27 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - check the code
+ * print_array - print an array of integers
+ * @a: an array of integers
+ * @n: the number of elements to swap
  *
- * Return: Always 0.
+ * Return: nothing.
  */
-int main(void)
-{
-    char *str;
 
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    print_rev(str);
-    return (0);
+void print_array(int *a, int n)
+{
+    int i;
+
+    i = 0;
+    while (i < n)
+    {
+        if (i != 0)
+        {
+            printf(", ");
+        }
+        printf("%d", a[i]);
+        i++;
+    }
+    printf("\n");
 }
