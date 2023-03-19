b9b10c22caefd1ca71b9902cabf41fbf7cc65edc
mau4512 <mauricio.25.17@gmail.com>
2023-03-19 10:07:40 -0700

    ejercicio 2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,67 @@
+#include "main.h"
+#include <stdlib.h>
+
+/*
+ * str_concat - A function that concatenates two strings
+ * @s1: An input pointer of the first string
+ * @s2: An input pointer of the second string
+ * Return: a pointer to concatened strings or NULL if it str is NULL
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *new_str;
+	char *start1;
+	char *start2;
+
+	int i;
+	int lens1 = 0;
+	int lens2 = 0;
+
+	start1 = s1;
+	start2 = s2;
+
+	if(s1 == NULL)
+	{
+		if(s2 == NULL)
+			return NULL;
+		return s2;
+	}
+	else if(s2 == NULL)
+		return s1;
+	else
+	{
+		while (*s1)
+		{
+			lens1++;
+			s1++;
+		}
+		s1 = start1;
+
+		while (*s2)
+		{
+			lens2++;
+			s2++;
+		}
+		
+		s2 = start2;
+
+		new_str = malloc(sizeof(char) * (lens1 + lens2 + 1));
+		start1 = new_str;
+		for (i = 0; i < (lens1 + lens2);i++)
+		{
+			if(i < lens1)
+			{
+				new_str[i] = *s1;
+				s1++;
+			}
+			else
+			{
+				new_str[i] = *s2;
+				s2++;
+			}
+		}
+		new_str[i] = '\0';
+	}
+	return new_str;
+}
--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid - A function that returns a pointer to a 
- * 2 dimensional array of integers
- * @width: An input integer at number of columns
- * @height: An input integer at number of rows
- * Return: pointer to a 2D array, NULL on failure
- */
+* alloc_grid - A function that returns a pointer to a 
+* 2 dimensional array of integers
+* @width: An input integer at number of columns
+* @height: An input integer at number of rows
+* Return: pointer to a 2D array, NULL on failure
+*/
 
 int **alloc_grid(int width, int height)
 {
