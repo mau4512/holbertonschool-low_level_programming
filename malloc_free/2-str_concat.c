ae439bea321b04571406060b82a739b00cc02d34
mau4512 <mauricio.25.17@gmail.com>
2023-03-19 10:22:13 -0700

    ejercicio 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,50 +10,38 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *new_str;
-	char *start1;
-	char *start2;
+	char *new_str, *start1, *start2;
 
-	int i;
-	int lens1 = 0;
-	int lens2 = 0;
+	int i, lens1 = 0, lens2 = 0;
 
 	start1 = s1;
 	start2 = s2;
 
-	if(s1 == NULL)
+	if (s1 == NULL)
 	{
 		s1 = "";
 	}
-
 	while (*s1)
 	{
 		lens1++;
 		s1++;
 	}
-
 	s1 = start1;
-
-	if(s2 == NULL)
+	if (s2 == NULL)
 	{
 		s2 = "";
 	}
-
 	while (*s2)
 	{
 		lens2++;
 		s2++;
 	}
-	
 	s2 = start2;
-
 	new_str = malloc(sizeof(char) * (lens1 + lens2 + 1));
-	
 	start1 = new_str;
-	
 	for (i = 0; i < (lens1 + lens2);i++)
 	{
-		if(i < lens1)
+		if (i < lens1)
 		{
 			new_str[i] = *s1;
 			s1++;
