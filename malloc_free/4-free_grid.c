96206b967eefe635a22eab1daa17f65b6bd496de
mau4512 <mauricio.25.17@gmail.com>
2023-03-19 11:31:11 -0700

    ejercicio 4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/*
+ * free_grid - A functuin that frees a 2D array if ints previously
+ * created by yout malloc_grid function
+ * @grid: an input 2D array of integers to free
+ * @height: height of grid
+ * Return: Nothing
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL)
+		return;
+	for(i = 0; i < height; i++)
+		free((int*)grid[i]);
+	free(grid);
+}
