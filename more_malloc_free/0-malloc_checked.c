d0ed5596201983ef54bc004eba688ebde80e6608
mau4512 <mauricio.25.17@gmail.com>
2023-03-22 10:52:31 -0700

    ejercicio 0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
 
-/*
- * *malloc_checked: A funtion that allocates memory using malloc.
- * @b: an unsigned input integer
- * Return: a pointer to allocated memory or NULL if it fails
- */
+/**
+  * malloc_checked: A funtion that allocates memory using malloc.
+  * @b: an unsigned input integer
+  * Return: a pointer to allocated memory or NULL if it fails
+  */
 
 void *malloc_checked(unsigned int b)
 {
