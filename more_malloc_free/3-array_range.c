f5a8c609c6819db18a7ef5b95a82775e2d2b5cbf
mau4512 <mauricio.25.17@gmail.com>
2023-03-23 13:31:21 -0700

    ejercicio 3

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - A function that creates an array of integers
+ * @min: an input integer that represents the minimum
+ * @max: an input integer that representes the maximum
+ * Return: A pointer to the newly array created or NULL 
+ */
+
+int *array_range(int min, int max)
+{
+	int *nuevo;
+	int dif, i;
+
+	if (min > max)
+		return (NULL);
+
+	dif = max - min;
+	nuevo = malloc((dif + 1) * sizeof(int));
+	if (nuevo == NULL)
+		return (NULL);
+
+	for (i = 0; i <= dif; i++)
+	{
+		nuevo[i] = min;
+		min++;
+	}
+	return (nuevo);
+}
