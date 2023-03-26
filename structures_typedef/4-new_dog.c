ee6f31851c989b8813a107388fabac5ed77c1b5c
mau4512 <mauricio.25.17@gmail.com>
2023-03-25 17:59:25 -0700

    ejercicio 4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,50 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * new_dog - a function that creates a new dog
+ * @name: an input string that has the name
+ * @age: an input float number that represents the age
+ * @owner: an input string that has the owner's name
+ * Return: NULL if fails or a pointer to the new structure
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	char *cname, *cowner;
+	int len_name = 0, len_owner = 0, i;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	while (name[len_name])
+		len_name++;
+
+	while (owner[len_owner])
+		len_owner++;
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+
+	cname = malloc(len_name + 1);
+	if (cname == NULL)
+		return (NULL);
+	for (i = 0; name[i]; i++)
+		cname[i] = name[i];
+	cname[i] = '\0';
+
+	cowner = malloc(len_owner + 1);
+	if (cowner == NULL)
+		return (NULL);
+	for (i = 0; owner[i]; i++)
+		cowner[i] = owner[i];
+	cowner[i] = '\0';
+
+	new_dog->name = cname;
+	new_dog->age = age;
+	new_dog->owner = cowner;
+	return (new_dog);
+}
+
