21cf6e6f67b01c36982cf662f938a786b1d074e2
mau4512 <mauricio.25.17@gmail.com>
2023-03-27 10:54:27 -0700

    ejercicio 5

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -11,9 +11,9 @@ void free_dog(dog_t *d)
 {
 	if (d == NULL)
 		return;
-	if(d->name != NULL)
+	if (d->name != NULL)
 		free(d->name);
-	if(d->owner != NULL)
+	if (d->owner != NULL)
 		free(d->owner);
 	free(d);
 }
