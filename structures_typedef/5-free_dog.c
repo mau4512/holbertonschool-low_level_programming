6578c6ecf98bf7b5ce109e56bc8c5c0cbed17e90
mau4512 <mauricio.25.17@gmail.com>
2023-03-27 10:53:35 -0700

    ejercicio 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - a function that frees a dog streucure
+ * @d: pointer to struct to free
+ * Return: Nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	if(d->name != NULL)
+		free(d->name);
+	if(d->owner != NULL)
+		free(d->owner);
+	free(d);
+}
