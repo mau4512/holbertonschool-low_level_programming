771a054d8449eb7ee2abb39d5570a0026b4d6a89
mau4512 <mauricio.25.17@gmail.com>
2023-04-14 13:02:04 -0700

    ejercicio 0

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 /**
- * truct hash_node_s - Node of a hash table
+ * struct hash_node_s - Node of a hash table
  *
  * @key: The key, string
  * The key is unique in the HashTable
@@ -17,7 +17,7 @@ typedef struct hash_node_s
 	char *key;
 	char *value;
 	struct hash_node_s *next;
-}hash_node_t;
+} hash_node_t;
 
 /**
  * struct hash_table_s - Hash table data structure
@@ -32,7 +32,7 @@ typedef struct hash_table_s
 {
 	unsigned long int size;
 	hash_node_t **array;
-}hash_table_t;
+} hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
 
