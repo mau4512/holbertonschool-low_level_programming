34828dd3dbe490f6e27e271151a3557238f713b1
mau4512 <mauricio.25.17@gmail.com>
2023-05-04 09:02:49 -0700

    ejercicio 5

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+  * get_dnodeint_at_index - Gets a node from a doubly linked list
+  * @head: The head of the doubly linked list
+  * @index: The index to find in the doubly linked list
+  *
+  * Return: The specific node of the doubly linked list
+  */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *actual = head;
+	unsigned int iterator = 0;
+
+	if (head)
+	{
+		while (actual != NULL)
+		{
+			if (iterator == index)
+				return (actual);
+
+			actual = actual->next;
+			++iterator;
+		}
+	}
+
+	return (NULL);
+}
