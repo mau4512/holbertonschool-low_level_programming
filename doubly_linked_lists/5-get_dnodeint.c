f6ed1925cc5e557e297114888baa8a46eb481ca0
mau4512 <mauricio.25.17@gmail.com>
2023-05-04 09:09:35 -0700

    ejercicio 5

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -4,7 +4,6 @@
   * get_dnodeint_at_index - Gets a node from a doubly linked list
   * @head: The head of the doubly linked list
   * @index: The index to find in the doubly linked list
-  *
   * Return: The specific node of the doubly linked list
   */
 
